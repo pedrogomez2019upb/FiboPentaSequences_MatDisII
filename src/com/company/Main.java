536d286860b7ce356caeee6eb52f0cf5cea6d1c7
Pedro Felipe <pedro.gomez.2019@upb.edu.co>
2020-01-27 07:47:31 -0500

    First Push

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+//#################################
+//MAIN CLASS
+//#################################
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //#################################
+        //Arithmethic Series
+        //#################################
+
+        //Let's start by
+	//
+    }
+}
+//Developed by Pedro Gomez
