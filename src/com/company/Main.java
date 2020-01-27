6322d9cf18f4b6bb1cecdca0d4e37a6cb4240df0
Pedro Felipe <pedro.gomez.2019@upb.edu.co>
2020-01-27 13:31:02 -0500

    Creation of diverse functions for the Pentagonal Sequence

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,27 +5,45 @@
 //develop an exercise , which consist of giving the product
 //of the sum of the "x" first
 package com.company;
-
+//##################################################################
+//Importing "Read" library
+//##################################################################
+//In Java we need to import a library so that we can read an
+//external input given by the user. For this , we are going
+//to import the "scanner" library.
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args) {
-        //#################################
-        //Pentagonal Series
-        //#################################
-
-        //Let's start by creating a function that
-        //creates a Pentagonal Sequence by the
-        //number i give to it.
-        //
-        //First, we are going to create the required variables
-        //so that the procedure can be done, this goes for
-        //counters, numbers, etc.
-        int count = 1;
+    //##################################################################
+    //Creating "For" cycle for the Pentagonal function, as another
+    //function.
+    //##################################################################
+    //Then, we are going to create the required variables
+    //so that the procedure can be done, this goes for
+    //counters, numbers, etc.
+    public static int generateSumPentagonal(int x){
+        int sum=0;
         int x=4;
+
         for(int i=1;i<=x;i++){
-            System.out.println(i);
+
         }
+    }
+
+    //##################################################################
+    //Creating Pentagonal function
+    //##################################################################
+    //First, we a going to create a Pentagonal function because we need to
+    //change the "n" number various times so that we can get the sum
+    public static int generatePentagonal(int n){
+
+        return (n * (3 * n - 1))/2;
+
+
+    }
+    public static void main(String[] args) {
+
+
 	//
     }
 }
-//Developed by Pedro Gomez
+//Developed by Pedro Gomez - 000396221
