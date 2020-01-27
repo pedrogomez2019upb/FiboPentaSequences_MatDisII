8c7127b1f527d394e740cd66bbade823e7e24f51
Pedro Felipe <pedro.gomez.2019@upb.edu.co>
2020-01-27 11:56:50 -0500

    Studying for cycle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,30 @@
 //#################################
 //MAIN CLASS
 //#################################
+//Hi to everyone ! In this proyect , we are goint to
+//develop an exercise , which consist of giving the product
+//of the sum of the "x" first
 package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
         //#################################
-        //Arithmethic Series
+        //Pentagonal Series
         //#################################
 
-        //Let's start by
+        //Let's start by creating a function that
+        //creates a Pentagonal Sequence by the
+        //number i give to it.
+        //
+        //First, we are going to create the required variables
+        //so that the procedure can be done, this goes for
+        //counters, numbers, etc.
+        int count = 1;
+        int x=4;
+        for(int i=1;i<=x;i++){
+            System.out.println(i);
+        }
 	//
     }
 }
